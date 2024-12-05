62de680b0ef3b8682ad2aaed3b552af5191a1e35
James <ismay.james@gmail.com>
2024-12-05 15:20:16 +0000

    Final Init

--- AdventOfCode2024/obj/Debug/net8.0/AdventOfCode2024.AssemblyInfo.cs
+++ AdventOfCode2024/obj/Debug/net8.0/AdventOfCode2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e58c0332fe0058aa94c79bc75bf454ba30439c01")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
